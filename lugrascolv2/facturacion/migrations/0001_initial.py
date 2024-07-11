c732bbbdaaffbc1fbef06a848419d2bc8ab8929f
KevinSanchez0522 <167906890+KevinSanchez0522@users.noreply.github.com>
2024-07-11 13:44:04 -0500

    update

--- lugrascolv2/facturacion/migrations/0001_initial.py
+++ lugrascolv2/facturacion/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 ('nit', models.DecimalField(db_column='Nit', decimal_places=10, max_digits=2, primary_key=True, serialize=False)),
                 ('nombre', models.TextField()),
                 ('direccion', models.TextField()),
-                ('telefono', models.DecimalField(decimal_places=65535, max_digits=65535)),
+                ('telefono', models.DecimalField(decimal_places=10, max_digits=2)),
                 ('email', models.TextField()),
             ],
             options={
